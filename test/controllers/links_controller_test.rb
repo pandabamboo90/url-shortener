4cd75b26975be43f6b356552dea00464349408a3
Dang Nguyen <dang.nguyen@comestri.com>
2022-11-25 11:13:43 +0700

    clean up & add tests for controller

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -5,34 +5,13 @@ class LinksControllerTest < ActionDispatch::IntegrationTest
     @link = links(:one)
   end
 
-  test "should get index" do
-    get links_url, as: :json
+  test "should response encoded URL" do
+    post encode_url, params: { url: 'https://valid_url.com' }, as: :json
     assert_response :success
   end
 
-  test "should create link" do
-    assert_difference('Link.count') do
-      post links_url, params: { link: { slug: @link.slug, url: @link.url } }, as: :json
-    end
-
-    assert_response 201
-  end
-
-  test "should show link" do
-    get link_url(@link), as: :json
+  test "should response decoded URL" do
+    post decode_url, params: { slug: @link.slug }, as: :json
     assert_response :success
-  end
-
-  test "should update link" do
-    patch link_url(@link), params: { link: { slug: @link.slug, url: @link.url } }, as: :json
-    assert_response 200
-  end
-
-  test "should destroy link" do
-    assert_difference('Link.count', -1) do
-      delete link_url(@link), as: :json
-    end
-
-    assert_response 204
   end
 end
