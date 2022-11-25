526fe3a5ae81fc030dec4c4efd6b063cb9c8aa67
Dang Nguyen <dang.nguyen@comestri.com>
2022-11-25 11:36:10 +0700

    refactor encode url service

--- app/services/encode_url_service.rb
+++ app/services/encode_url_service.rb
@@ -6,14 +6,13 @@ class EncodeUrlService
   def call
     link = Link.find_by(url: @url)
 
-    if link.present?
-      encoded_url = link.encoded_url
-    else
-      encoded_url = SecureRandom.uuid[0..5]
-      Link.create!(url: @url, encoded_url: encoded_url)
+    if link.blank?
+      encoded_domain = [SecureRandom.uuid[0..5], SecureRandom.uuid[0..2]].join('.')
+      url_scheme = @url.split('//').first
+      link = Link.create!(url: @url, encoded_url: [url_scheme, encoded_domain].join('//'))
     end
 
     # Return the encoded_url
-    encoded_url
+    link.encoded_url
   end
 end
